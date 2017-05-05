42ba41f35060f2185a3c20abc62f8abfd273e8ce
Krzysztof Atłasik <krzysztof.atlasik@corp.smtsoftware.com>
2017-05-05 16:14:23 +0200

    Added translations.

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+import getParameter from 'get-parameter';
 import template from '@/template.html'
 import main from '@/styles/main.scss'
 import fontAwesome from "font-awesome-webpack"
+import mugshot from "@/images/mugshot.jpg"
+import data from "@/data.yaml"
+import openSans from "@/fonts/OpenSans-Regular.ttf"
+import moment from "moment"
+import Handlebars from "handlebars"
 
 
-document.querySelector("main").innerHTML = template({msg: "Witaj"})
+const locale = getParameter("lang") || "pl"
+
+const messages = require(`@/messages_${locale}.yaml`)
+
+moment.locale(locale)
+
+
+Handlebars.registerHelper('formatDate', function(value) {
+  return moment(value, "DD.MM.YYYY").format("D MMMM YYYY")
+});
+
+Handlebars.registerHelper('formatDateShort', function(value) {
+  return moment(value, "DD.MM.YYYY").format("MMMM YYYY")
+});
+
+const model = {
+  mugshot,
+  data,
+  messages
+}
+
+const main = document.createElement("main")
+main.innerHTML = template(model)
+document.querySelector("body").appendChild(main)
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,65 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+const isProd = process.argv.indexOf('-p') !== -1;
+
+const sassProd = {
+  test: /\.scss$/,
+  loader: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: 'css-loader!sass-loader'
+  })
+}
+
+const sassDev =   {
+    test: /\.scss$/,
+    loaders: [
+      'style-loader',
+      'css-loader',
+      'sass-loader'
+    ]
+  }
+
+const sassLoader = isProd ? sassProd : sassDev
 
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname),
-    publicPath: "/",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
     filename: 'bundle.js'
   },
-  resolve:{
+  resolve: {
     alias: {
-      '@': path.resolve(__dirname)
+      '@': path.resolve(__dirname),
+      'handlebars': 'handlebars/runtime.js'
     }
+  },
+  resolveLoader: {
+    alias: {
+      'hbs': 'handlebars-loader'
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new ExtractTextPlugin("css/[name].css")
+  ],
+  node: {
+    fs: "empty"
   },
   module: {
     loaders: [
       {
+        test: /\.yaml$/,
+        loaders: [
+          'json-loader',
+          'yaml-loader'
+        ]
+      },
+      {
         test: /\.html$/,
-        loaders: ['mustache-loader']
+        loaders: ['handlebars-template-loader']
       },
       {
         test: /\.css$/,
@@ -26,19 +69,19 @@ module.exports = {
           'postcss-loader'
         ]
       },
+      sassLoader,
       {
-        test: /\.scss$/,
-        loaders: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },{
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader?name=js/fonts/[name].[ext]"
+        loader: "file-loader?name=fonts/[name].[ext]"
      },{
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: "url-loader"
+      },{
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=images/[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug=true&quality=100'
+        ]
       }
     ]
   }
