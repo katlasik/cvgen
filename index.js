e06d44707ce7a60597aba470335aacb2d5824566
Krzysztof Atłasik <krzysztofatlasik@gmail.com>
2017-07-11 01:41:19 +0200

    Added phantom js. Sidebar moved to seperate template.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import getParameter from 'get-parameter';
+import getParameter from 'get-parameter'
 import template from '@/template.html'
+import sidebar from '@/sidebar.html'
+
 import main from '@/styles/main.scss'
 import fontAwesome from "font-awesome-webpack"
 import mugshot from "@/images/mugshot.jpg"
@@ -7,6 +9,9 @@ import mugshot from "@/images/mugshot.jpg"
 import openSans from "@/fonts/OpenSans-Regular.ttf"
 import moment from "moment"
 import Handlebars from "handlebars"
+
+import conf from "@/conf/properties.yaml"
+import { range } from "lodash"
 
 
 const locale = getParameter("lang") || "pl"
@@ -19,11 +24,28 @@ moment.locale(locale)
 
 Handlebars.registerHelper('formatDate', function(value) {
   return moment(value, "DD.MM.YYYY").format("D MMMM YYYY")
-});
+})
 
 Handlebars.registerHelper('formatDateShort', function(value) {
   return moment(value, "DD.MM.YYYY").format("MMMM YYYY")
-});
+})
+
+Handlebars.registerHelper('times', function(n, block) {
+    return range(n).map (i => {
+        block.data.index = i
+        block.data.first = i === 0
+        block.data.last = i === (n - 1)
+        return block.fn(this)
+    })
+})
+
+Handlebars.registerHelper('rating', function(n) {
+    return range(conf.maximum_rating).map(i =>
+      (i <= n) ? "<i class='fa fa-star'></i>" : "<i class='fa fa-star-o'></i>"
+    ).join(" ")
+})
+
+Handlebars.registerPartial('sidebar', sidebar)
 
 const model = {
   mugshot,
